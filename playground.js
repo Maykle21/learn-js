766c3779fa8c421bfce5aa8256d40bc2c8419c9d
Maykle21 <38420358+Maykle21@users.noreply.github.com>
2018-05-17 10:23:18 +0300

    Update playground.js

--- playground.js
+++ playground.js
@@ -1,14 +1,3 @@
-/*\
-cd - change dir
-ls - list directory content
-git clone - clone repository
-cat -  provide content or type and size information for repository objects
-rm - delete something
-git add - add all files to staging area
-git status - get git status 
-git commit -m "commit message" - create commit git diff
-*/
-
 var assertEquals = (expected, actual) => {
     if (expected != actual)
         throw `Expected ${expected}, but got ${actual}`;
